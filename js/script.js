c85af2de5cee61d0bc671b40bd45ba4349f5097c
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-08 16:04:09 +0200

    freccia sinistra

--- js/script.js
+++ js/script.js
@@ -9,9 +9,30 @@ chevronRight.click(function(){
     console.log(activeElement);
     activeElement.removeClass('active');
 
+    //individuo con .next elemento successivo e gli aggiungo class active per mostrare elemento 
     var nextElement = activeElement.next();
     //console.log(nextElement);
     nextElement.addClass('active');
+
+});
+
+//al click della freccia sinistra l' immagine deve scorrere a quella precedente
+var chevronLeft = $('.chevron.left');
+//console.log(chevronLeft);
+
+chevronLeft.click(function(){
+    //creo una variabile che individua classe active
+    //rimuovo classe active
+    var activeElement = $('.active');
+    console.log(activeElement);
+    activeElement.removeClass('active');
+    console.log(activeElement);
+
+    //individuo con .prev elemento precedente e gli aggiungo class active per mostrare elemento 
+    var prevElement = activeElement.prev();
+    console.log(prevElement);
+    prevElement.addClass('active');
+    
 
 });
 
